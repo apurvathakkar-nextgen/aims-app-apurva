771f1652d3aea853835fab4796ab7feb80a9a384
Apurva Thakkar <apurva.thakkar@nextgenus.ai>
2025-02-23 16:01:38 -0800

    Combine troubleshoot combine for deployment 39

--- lib/amplify-stack-backup.ts
+++ lib/amplify-stack-backup.ts
@@ -1,40 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as amplify from '@aws-cdk/aws-amplify-alpha';
-import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 export class AmplifyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // Custom buildSpec that returns a YAML string
-    const myBuildSpec: codebuild.BuildSpec = {
-      isImmediate: false,
-      toBuildSpec: () => `
-version: "1"
-frontend:
-  phases:
-    preBuild:
-      commands:
-        - cd amplify
-        - ls
-        - cd..
-        - cd frontend
-        - npm ci
-        - ls ./src
-        - cp amplify_outputs.json ./src/amplify_outputs.json
-    build:
-      commands:
-        - npm run build
-  artifacts:
-    baseDirectory: frontend/build
-    files:
-      - "**/*"
-  cache:
-    paths:
-      - frontend/node_modules/**/*
-`
-    };
 
     const amplifyApp = new amplify.App(this, 'AIMS-App', {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
@@ -42,7 +12,7 @@ frontend:
         repository: 'aims-app-apurva',
         oauthToken: cdk.SecretValue.secretsManager('github-token'),
       }),
-      buildSpec: myBuildSpec,
+      // No buildSpec property (using default build settings)
     });
 
     const mainBranch = amplifyApp.addBranch('main');
